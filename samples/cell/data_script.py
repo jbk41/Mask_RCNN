4100821c3952841e94e7e57c8511d4e25e9623ce
Justin Kim <jbk41@duke.edu>
2018-07-12 16:08:49 -0400

    imgmask, reset, split, and img commands all work

--- samples/cell/data_script.py
+++ samples/cell/data_script.py
@@ -1,4 +1,5 @@
-from data import np_to_image
+from data import np_to_imgmask
+from data import np_to_img
 from data import train_test_split 
 from data import reset
 import os
@@ -9,32 +10,40 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate data, train/test split, reset train/test splits')
     parser.add_argument("command",
                         metavar="<command>",
-                        help="either 'imgmask' 'split' 'reset'")
-    parser.add_argument('--imgdir', required=True,
-                        metavar="/path/to/image/directory/",
-                        help='Directory of the np images')
+                        help="either 'imgmask', 'img', 'split', 'reset'")
+    parser.add_argument('--outdir', required=True,
+                        metavar="/path/to/output/directory/",
+                        help='Output directory of the np images')
     parser.add_argument('--images', required=False,
-                        metavar="Image numpy file",
-                        help='Name of image numpy file in imgdir')
+                        metavar="/path/to/numpy/image/file",
+                        help='Path to numpy image file')
     parser.add_argument('--masks', required=False,
-                        metavar="Mask numpy file",
-                        help='Name of mask numpy file in imgdir')
+                        metavar="/path/to/numpy/mask/file",
+                        help='Path to numpy mask file')
     parser.add_argument('--train-percent', required=False,
                         default=.7,
                         metavar='train percentage as decimal value',
                         help='Percentage of data to allocate for training')
     args = parser.parse_args()
 
-    train_directory = os.path.join(imgdir, 'training_data')
-    test_directory = os.path.join(imgdir, 'testing_data')
+    
+    assert args.command in ['imgmask', 'img', 'split', 'reset'], "command must be in ['imgmask', 'img', 'split', 'reset']"
+
+    train_directory = os.path.join(args.outdir, 'training_data')
+    test_directory = os.path.join(args.outdir, 'testing_data')
          
     if args.command == 'imgmask':
         assert [args.images, args.masks], 'imgmask requires the --images and --masks arguments'
-        np_to_image(args.images, args.masks, args.imgdir)   
+        np_to_imgmask(args.images, args.masks, args.outdir)   
+    
+    if args.command == 'img':
+        assert args.images, "img requires the --images argument"    
+        np_to_img(args.images, args.outdir) 
     
     if args.command == 'split':
-        train_test_split(train_directory, test_directory, args.imgdir, args.train_percent)
+        train_test_split(train_directory, test_directory, args.outdir, args.train_percent)
 
     if args.command == 'reset':
-        reset(train_directory, test_directory, args.imgdir)
-        
+        reset(train_directory, test_directory, args.outdir)
+       
+     
