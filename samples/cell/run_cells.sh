2cc8757f617507cffbea465828e2c1ccff4bedd0
Justin Kim <jbk41@duke.edu>
2018-07-13 11:38:28 -0400

    change python script line to run cell_script.py instead of cells.py

--- samples/cell/run_cells.sh
+++ samples/cell/run_cells.sh
@@ -5,4 +5,8 @@ module load cuDNN/7.0/CUDA-9.0
 source /data/kimjb/conda/etc/profile.d/conda.sh
 conda activate mask_rcnn
 
-python cells.py
+# python cells.py
+
+python cell_script.py train --dataset=/data/kimjb/Mask_RCNN/image_test/images ---init=coco --logs=/data/kimjb/Mask_RCNN/logs 
+
+echo 'Done'
